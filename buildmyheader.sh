3a4c2faa4ef3fda07b4baf618d3100f458ca6d52
LiorF <liorfe@localhost.localdomain>
2018-12-25 16:00:35 -0500

    create an script

---
+++ buildmyheader.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+echo "Enter script name:"
+read script_name
+if [ -e $script_name ]; then
+   echo "File $script_name already exists!"
+   exit 0
+fi
+echo "Enter your name:"
+read dev_name
+echo "Enter description:"
+read script_desc
+
+touch $script_name
+chmod 755 $script_name
+
+cat >$script_name  <<EOF
+#!/usr/bin/env bash
+#####################
+#Author : $dev_name   #
+#Date : $DATE       #
+#Description: $script_desc   #
+######################"
+EOF
+
+printf "%sScript $script_name created successfully\n"
+
+exit 0
